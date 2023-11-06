9a2e70f3345f7622e87cd7ceb135a38e4711b6e0
Arshia12of <87532076+Arshia12of@users.noreply.github.com>
2023-11-05 23:21:40 -0500

    3

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 const express = require('express')
+const fs = require("fs");
 const app = express()
+
+const path = "./chats.json";
+
+fs.readFile(path, "utf8", (error, data) => {
+    if (error) {
+      console.log(error);
+      return;
+    }
+    console.log(JSON.parse(data));
+  });
+
+
+
+
 app.all('/', (req, res) => {
+
     console.log("got a request!")
     res.send('Yo')
 })
