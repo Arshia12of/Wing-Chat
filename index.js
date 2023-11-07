8c921cb657a3a58efc1803d8a5bc756cb3eac3d5
Arshia12of <87532076+Arshia12of@users.noreply.github.com>
2023-11-07 13:03:40 -0500

    10.3

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ const app = express();
 const path = "./chats.json";
 var readedData;
 
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
 fs.readFile(path, "utf8", (error, data) => {
     if (error) {
       console.log(error);
@@ -21,6 +26,8 @@ app.all('/', (req, res) => {
     console.log("request token")
     // res.send(req.header('x-forwarded-for'))
     // res.send(readedData)
-    res.sendFile('/index.html');
+    res.sendFile('index.html');
 })
+
+
 app.listen(process.env.PORT || 3000)
